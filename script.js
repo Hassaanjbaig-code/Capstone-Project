7c3aa419c23565c11d9f81258b66437785a6a1d0
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-11-24 01:08:17 +0500

    Add Main index

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('#show');
+const icon = document.querySelector('.nav-icon');
+const close = document.querySelector('.close')
+
+icon.addEventListener('click' , () => {
+  nav.classList.add('show');
+})
+
+close.addEventListener('click' , () => {
+    nav.classList.remove('show');
+})
