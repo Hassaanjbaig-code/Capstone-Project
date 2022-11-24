7aa90d82bdbcba081c9b2127893d027bf0681752
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-11-24 09:27:16 +0500

    Add function to navbar

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const nav = document.querySelector('#show');
 const icon = document.querySelector('.nav-icon');
 const close = document.querySelector('.close');
+const navbar = document.querySelectorAll('.navbar-fun');
 
 icon.addEventListener('click', () => {
   nav.classList.add('show');
 });
+
+for (let i = 0; i < navbar.length; i += 1) {
+  navbar[i].addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
+}
 
 close.addEventListener('click', () => {
   nav.classList.remove('show');
